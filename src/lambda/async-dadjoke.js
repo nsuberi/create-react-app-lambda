9a3ffb0030970d87bd4e200571a43a091725a979
nsuberi <nsuberi@gmail.com>
2019-05-21 18:01:09 -0400

    switch to RDS connection

--- src/lambda/async-dadjoke.js
+++ src/lambda/async-dadjoke.js
@@ -1,25 +1,49 @@
 // example of async handler using async-await
 // https://github.com/netlify/netlify-lambda/issues/43#issuecomment-444618311
 
-import fetch from "node-fetch"
-export async function handler(event, context) {
-  try {
-    const response = await fetch("https://icanhazdadjoke.com", { headers: { Accept: "application/json" } })
-    if (!response.ok) {
-      // NOT res.status >= 200 && res.status < 300
-      return { statusCode: response.status, body: response.statusText }
-    }
-    const data = await response.json()
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ msg: data.joke })
-    }
-  } catch (err) {
-    console.log(err) // output to netlify function log
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
-    }
+const mysql = require('serverless-mysql')({
+  config: {
+    host     : 'trialdb.c7ksi0dlwx8a.us-east-2.rds.amazonaws.com',
+    database : 'dbname',
+    user     : 'dbuser',
+    password : 'password'
   }
+})
+
+// Main handler function
+exports.handler = async (event, context) => {
+  // Run your query
+  let results = await mysql.query('SELECT * FROM item_info')
+
+  // Run clean up function
+  await mysql.end()
+
+  // Return the results
+  return {
+        statusCode: 200,
+        body: JSON.stringify({ msg: results })
+      }
 }
+
+// import fetch from "node-fetch"
+// export async function handler(event, context) {
+//   try {
+//     const response = await fetch("https://icanhazdadjoke.com", { headers: { Accept: "application/json" } })
+//     if (!response.ok) {
+//       // NOT res.status >= 200 && res.status < 300
+//       return { statusCode: response.status, body: response.statusText }
+//     }
+//     const data = await response.json()
+//
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({ msg: data.joke })
+//     }
+//   } catch (err) {
+//     console.log(err) // output to netlify function log
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
+//     }
+//   }
+// }
